13d45ecada6ec29de9c39856ff21c6648dcc0ed8
HaoxuanXu <haoxuanxu@uchicago.edu>
2019-08-31 23:53:44 -0500

    index trend api calling

--- Get_Stock_Market_Info.py
+++ Get_Stock_Market_Info.py
@@ -1,18 +1,41 @@
 #from libraries
 import requests
+import pandas as pd
+import pickle
+import json
 
 
 #from files:
 import auth
 
 # url_base:
-alpha-vantage-base = "https://www.alphavantage.co"
+alpha_vantage_base = "https://www.alphavantage.co"
 
 #initialization:
-params = {
-    "function":"Time_Series_Daily_Adjusted",
-    "symbol":"VOO",
-    "outputsize":"compact",
-    "datatype":"csv",
-    "apikey":auth.apikey
-}
+fund_symbols = ['VFINX', 'VTSMX', 'FXAIX', 'SWTSX']
+#Extract 20-year historical data for ETF that correspond to Dow Jones, S&P 500, Gold, US Bond Market, and US Dollar Index
+technical_indicators = ['SMA', 'EMA', 'MACD', 'STOCH', 'RSI', 'ADX', 'MOM', 'BOP', 'CCI', 'ROC', 'AROON', 'MFI',
+                        'DX', 'BBANDS', 'AD', 'OBV']
+
+
+
+
+def get_index_trend(fund_symbols, outputsize, apikey):
+    for fund in fund_symbols:
+        print('Getting 20-year daily historical data for {}'.format(fund))
+        params = {
+            "function": "Time_Series_Daily",
+            "symbol": fund,
+            "outputsize": outputsize,
+            "datatype": "json",
+            "apikey": apikey
+        }
+        r = requests.get(alpha_vantage_base + '/query', params=params)
+        time_series_daily = pd.DataFrame.from_dict(json.loads(r.content)['Time Series (Daily)'], orient='index')
+        return time_series_daily
+
+VFINX, VTSMX, FXAIX, SWRSX = get_index_trend(fund_symbols=fund_symbols, outputsize='full', apikey=auth.apikey)
+
+
+def get_indicators():
+    
