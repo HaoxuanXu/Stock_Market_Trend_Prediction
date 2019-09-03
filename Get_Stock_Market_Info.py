b18a956af3ca57b1086894a3c811c688c3e46bf0
HaoxuanXu <haoxuanxu@uchicago.edu>
2019-09-02 19:37:40 -0500

    troubleshooting

--- Get_Stock_Market_Info.py
+++ Get_Stock_Market_Info.py
@@ -2,9 +2,10 @@
 # from libraries
 import requests
 import pandas as pd
-import pickle
 import json
+import os
 import quandl
+import datetime as dt
 
 # from files:
 import auth
@@ -12,7 +13,7 @@ import params
 
 # url_base:
 alpha_vantage_base = "https://www.alphavantage.co"
-
+raw_data_folder = "C:/Users/John Xu/Desktop/Stock_Market_Trend_Prediction/AlphaVantage_data/"
 # initialization:
 fund_symbols = ['VFINX', 'VTSMX', 'FXAIX', 'SWTSX']
 # Extract 20-year historical data for ETF that correspond to Dow Jones, S&P 500, Gold, US Bond Market, and US Dollar
@@ -20,21 +21,58 @@ fund_symbols = ['VFINX', 'VTSMX', 'FXAIX', 'SWTSX']
 technical_indicators = ['SMA', 'EMA', 'MACD', 'STOCH', 'RSI', 'ADX', 'MOM', 'BOP', 'CCI', 'ROC', 'AROON', 'MFI',
                         'DX', 'BBANDS', 'SAR', 'TRANGE', 'AD', 'OBV']
 
+index_fund_params = params.index_fund_params
+SMA_open_params = params.SMA_open_params
+SMA_close_params = params.SMA_close_params
+EMA_open_params = params.EMA_open_params
+EMA_close_params = params.EMA_close_params
+MACD_open_params = params.MACD_open_params
+MACD_close_params = params.MACD_close_params
+STOCH_params = params.STOCH_params
+RSI_open_params = params.RSI_open_params
+RSI_close_params = params.RSI_close_params
+ADX_params = params.ADX_params
+MOM_open_params = params.MOM_open_params
+MOM_close_params = params.MOM_close_params
+BOP_params = params.BOP_params
+CCI_params = params.CCI_params
+ROC_open_params = params.ROC_open_params
+ROC_close_params = params.ROC_close_params
+AROON_params = params.AROON_params
+MFI_params = params.MFI_params
+DX_params = params.DX_params
+BBANDS_open_params = params.BBANDS_open_params
+BBANDS_close_params = params.BBANDS_close_params
+SAR_params = params.SAR_params
+TRANGE_params = params.TRANGE_params
+AD_params = params.AD_params
+OBV_params = params.OBV_params
+HT_TRENDLINE_open_params = params.HT_TRENDLINE_open_params
+HT_TRENDLINE_close_params = params.HT_TRENDLINE_close_params
+param_list = [index_fund_params, SMA_open_params, SMA_close_params, EMA_open_params, EMA_close_params,
+              MACD_open_params, MACD_close_params, STOCH_params, RSI_open_params, RSI_close_params,
+              ADX_params, MOM_open_params, MOM_close_params, BOP_params, CCI_params,
+              ROC_open_params, ROC_close_params, AROON_params, MFI_params, DX_params, BBANDS_open_params,
+              BBANDS_close_params,
+              SAR_params, TRANGE_params, AD_params, OBV_params, HT_TRENDLINE_open_params,
+              HT_TRENDLINE_close_params]
 
-def get_index_time_series(fund_symbols, outputsize, function, apikey):
+
+def get_alpha_vantage_data(fund_symbols, param_list):
     for fund in fund_symbols:
         print('Getting data for {}...'.format(fund))
-        index_params = params.index_params.update(symbol=fund)
-        SMA_params = params.index_params.update(symbol=fund)
-        EMA_open_params = params.EMA_open_params.update(symbol=fund)
-        EMA_close_params = params.EMA_close_params.update(symbol=fund)
-
-        r = requests.get(alpha_vantage_base + '/query', params=index_params)
-        time_series_daily = pd.DataFrame.from_dict(json.loads(r.content)['Time Series (Daily)'], orient='index')
-        return time_series_daily
-
-
-VFINX, VTSMX, FXAIX, SWRSX = get_index_time_series(fund_symbols=fund_symbols, outputsize='full', apikey=auth.apikey)
-
+        for param in param_list:
+            param["symbol"] = fund
+            print(param)
+            r = requests.get(alpha_vantage_base + '/query', params=param)
+            content = json.loads(r.content)
+            fund_prefix = ''.join((fund, "_"))
+            file_name = ''.join((fund_prefix, (param["function"] + '.json')))
+            with open(os.path.join(raw_data_folder, file_name), "w") as fp:
+                json.dump(content, fp)
+        print("Data extraction for {} complete!".format(fund))
 
 
+print('Alpha Vantage Data Extraction Process Complete!!')
+
+get_alpha_vantage_data(fund_symbols=fund_symbols, param_list=param_list)
--- params.py
+++ params.py
@@ -15,7 +15,7 @@ For Index Funds:
     Fidelity 500 Index Fund (FXAIX),
     Schwab Total Stock Market Index Fund (SWTSX)
 '''
-index_params = {
+index_fund_params = {
     "function": "Time_Series_Daily",
     "outputsize": "full",
     "datatype": "json",
@@ -28,24 +28,27 @@ For External Factors (Commodity Price, Exchange Rate) --using Quandl:
     Wiki Continuous Futures (Minneapolis NCI National Corn Futures, Continuous Contract #1 (IC1) (Front Month))--"US_Corn_Futures_params"
 '''
 US_Petro_params = {
-    "database_code":"GPP",
-    "dataset_code":"CFP_USA",
-    "start_date":str(dt.datetime(year=dt.datetime.now()-20, month=dt.datetime.now(), day=dt.datetime.now())),
-    "end_date":str(dt.datetime.now().strftime("%Y-%m-%d"))
+    "database_code": "GPP",
+    "dataset_code": "CFP_USA",
+    "start_date": str(dt.datetime(year=dt.datetime.now().year - 20, month=dt.datetime.now().month,
+                                  day=dt.datetime.now().day)),
+    "end_date": str(dt.datetime.now().strftime("%Y-%m-%d"))
 }
 
 US_Soybean_Futures_params = {
-    "database_code":"CHRIS",
-    "dataset_code":"MGEX_IS1",
-    "start_date":str(dt.datetime(year=dt.datetime.now()-20, month=dt.datetime.now(), day=dt.datetime.now())),
-    "end_date":str(dt.datetime.now().strftime("%Y-%m-%d"))
+    "database_code": "CHRIS",
+    "dataset_code": "MGEX_IS1",
+    "start_date": str(dt.datetime(year=dt.datetime.now().year - 20, month=dt.datetime.now().month,
+                                  day=dt.datetime.now().day)),
+    "end_date": str(dt.datetime.now().strftime("%Y-%m-%d"))
 }
 
 US_Corn_Futures_params = {
-    "database_code":"CHRIS",
-    "dataset_code":"MGEX_IC1",
-    "start_date":str(dt.datetime(year=dt.datetime.now()-20, month=dt.datetime.now(), day=dt.datetime.now())),
-    "end_date":str(dt.datetime.now().strftime("%Y-%m-%d"))
+    "database_code": "CHRIS",
+    "dataset_code": "MGEX_IC1",
+    "start_date": str(dt.datetime(year=dt.datetime.now().year - 20, month=dt.datetime.now().month,
+                                  day=dt.datetime.now().day)),
+    "end_date": str(dt.datetime.now().strftime("%Y-%m-%d"))
 }
 '''
 For Technical Indicators:
@@ -70,196 +73,196 @@ For Technical Indicators:
     Hilbert Transform, Instantaneous Trendline (HT_TRENDLINE) --daily open & daily close
 '''
 SMA_open_params = {
-    "function":"SMA",
-    "interval":"daily",
-    "time_period":200,
-    "series_type":"open",
-    "apikey":auth.apikey_av
+    "function": "SMA",
+    "interval": "daily",
+    "time_period": 200,
+    "series_type": "open",
+    "apikey": auth.apikey_av
 }
 
 SMA_close_params = {
-    "function":"SMA",
-    "interval":"daily",
-    "time_period":200,
-    "series_type":"close",
-    "apikey":auth.apikey_av
+    "function": "SMA",
+    "interval": "daily",
+    "time_period": 200,
+    "series_type": "close",
+    "apikey": auth.apikey_av
 }
 
 EMA_open_params = {
-    "function":"EMA",
-    "interval":"daily",
-    "time_period":200,
-    "series_type":"open",
-    "apikey":auth.apikey_av
+    "function": "EMA",
+    "interval": "daily",
+    "time_period": 200,
+    "series_type": "open",
+    "apikey": auth.apikey_av
 }
 
 EMA_close_params = {
-    "function":"EMA",
-    "interval":"daily",
-    "time_period":200,
-    "series_type":"close",
-    "apikey":auth.apikey_av
+    "function": "EMA",
+    "interval": "daily",
+    "time_period": 200,
+    "series_type": "close",
+    "apikey": auth.apikey_av
 }
 
 MACD_open_params = {
-    "function":"MACD",
-    "interval":"daily",
-    "series_type":"open",
-    "apikey":auth.apikey_av
+    "function": "MACD",
+    "interval": "daily",
+    "series_type": "open",
+    "apikey": auth.apikey_av
 }
 
 MACD_close_params = {
-    "function":"MACD",
-    "interval":"daily",
-    "series_type":"close",
-    "apikey":auth.apikey_av
+    "function": "MACD",
+    "interval": "daily",
+    "series_type": "close",
+    "apikey": auth.apikey_av
 }
 
 STOCH_params = {
-    "function":"STOCH",
-    "interval":"daily",
-    "apikey":auth.apikey_av
+    "function": "STOCH",
+    "interval": "daily",
+    "apikey": auth.apikey_av
 }
 
 RSI_open_params = {
-    "function":"RSI",
-    "interval":"daily",
-    "time_period":200,
-    "series_type":"open",
-    "apikey":auth.apikey_av
+    "function": "RSI",
+    "interval": "daily",
+    "time_period": 200,
+    "series_type": "open",
+    "apikey": auth.apikey_av
 }
 
 RSI_close_params = {
-    "function":"RSI",
-    "interval":"daily",
-    "time_period":200,
-    "series_type":"close",
-    "apikey":auth.apikey_av
+    "function": "RSI",
+    "interval": "daily",
+    "time_period": 200,
+    "series_type": "close",
+    "apikey": auth.apikey_av
 }
 
 ADX_params = {
-    "function":"ADX",
-    "interval":"daily",
-    "time_period":200,
-    "apikey":auth.apikey_av
+    "function": "ADX",
+    "interval": "daily",
+    "time_period": 200,
+    "apikey": auth.apikey_av
 }
 
 MOM_open_params = {
-    "function":"MOM",
-    "interval":"daily",
-    "time_period":200,
-    "series_type":"open",
-    "apikey":auth.apikey_av
+    "function": "MOM",
+    "interval": "daily",
+    "time_period": 200,
+    "series_type": "open",
+    "apikey": auth.apikey_av
 }
 
 MOM_close_params = {
-    "function":"MOM",
-    "interval":"daily",
-    "time_period":200,
-    "series_type":"close",
-    "apikey":auth.apikey_av
+    "function": "MOM",
+    "interval": "daily",
+    "time_period": 200,
+    "series_type": "close",
+    "apikey": auth.apikey_av
 }
 
 BOP_params = {
-    "function":"BOP",
-    "interval":"daily",
-    "apikey":auth.apikey_av
+    "function": "BOP",
+    "interval": "daily",
+    "apikey": auth.apikey_av
 }
 
 CCI_params = {
-    "function":"CCI",
-    "interval":"daily",
-    "time_period":200,
-    "apikey":auth.apikey_av
+    "function": "CCI",
+    "interval": "daily",
+    "time_period": 200,
+    "apikey": auth.apikey_av
 }
 
 ROC_open_params = {
-    "function":"ROC",
-    "interval":"daily",
-    "time_period":200,
-    "series_type":"open",
-    "apikey":auth.apikey_av
+    "function": "ROC",
+    "interval": "daily",
+    "time_period": 200,
+    "series_type": "open",
+    "apikey": auth.apikey_av
 }
 
 ROC_close_params = {
-    "function":"ROC",
-    "interval":"daily",
-    "time_period":200,
-    "series_type":"close",
-    "apikey":auth.apikey_av
+    "function": "ROC",
+    "interval": "daily",
+    "time_period": 200,
+    "series_type": "close",
+    "apikey": auth.apikey_av
 }
 
 AROON_params = {
-    "function":"AROON",
-    "interval":"daily",
-    "time_period":200,
-    "apikey":auth.apikey_av
+    "function": "AROON",
+    "interval": "daily",
+    "time_period": 200,
+    "apikey": auth.apikey_av
 }
 
 MFI_params = {
-    "function":"MFI",
-    "interval":"daily",
-    "time_period":200,
-    "apikey":auth.apikey_av
+    "function": "MFI",
+    "interval": "daily",
+    "time_period": 200,
+    "apikey": auth.apikey_av
 }
 
 DX_params = {
-    "function":"DX",
-    "interval":"daily",
-    "time_period":200,
-    "apikey":auth.apikey_av
+    "function": "DX",
+    "interval": "daily",
+    "time_period": 200,
+    "apikey": auth.apikey_av
 }
 
 BBANDS_open_params = {
-    "function":"BBANDS",
-    "interval":"daily",
-    "time_period":200,
-    "series_type":"open",
-    "apikey":auth.apikey_av
+    "function": "BBANDS",
+    "interval": "daily",
+    "time_period": 200,
+    "series_type": "open",
+    "apikey": auth.apikey_av
 }
 
 BBANDS_close_params = {
-    "function":"BBANDS",
-    "interval":"daily",
-    "time_period":200,
-    "series_type":"close",
-    "apikey":auth.apikey_av
+    "function": "BBANDS",
+    "interval": "daily",
+    "time_period": 200,
+    "series_type": "close",
+    "apikey": auth.apikey_av
 }
 
 SAR_params = {
-    "function":"SAR",
-    "interval":"daily",
-    "apikey":auth.apikey_av
+    "function": "SAR",
+    "interval": "daily",
+    "apikey": auth.apikey_av
 }
 
 TRANGE_params = {
-    "function":"TRANGE",
-    "interval":"daily",
-    "apikey":auth.apikey_av
+    "function": "TRANGE",
+    "interval": "daily",
+    "apikey": auth.apikey_av
 }
 
 AD_params = {
-    "function":"AD",
-    "interval":"daily",
-    "apikey":auth.apikey_av
+    "function": "AD",
+    "interval": "daily",
+    "apikey": auth.apikey_av
 }
 
 OBV_params = {
-    "function":"OBV",
-    "interval":"daily",
-    "apikey":auth.apikey_av
+    "function": "OBV",
+    "interval": "daily",
+    "apikey": auth.apikey_av
 }
 
 HT_TRENDLINE_open_params = {
-    "function":"HT_TRENDLINE",
-    "interval":"daily",
-    "series_type":"open",
-    "apikey":auth.apikey_av
+    "function": "HT_TRENDLINE",
+    "interval": "daily",
+    "series_type": "open",
+    "apikey": auth.apikey_av
 }
 
 HT_TRENDLINE_close_params = {
-    "function":"HT_TRENDLINE",
-    "interval":"daily",
-    "series_type":"close",
-    "apikey":auth.apikey_av
+    "function": "HT_TRENDLINE",
+    "interval": "daily",
+    "series_type": "close",
+    "apikey": auth.apikey_av
 }
