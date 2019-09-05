722e68c13cd88084c0f8c64bc0d7c210a536a028
HaoxuanXu <haoxuanxu@uchicago.edu>
2019-09-04 22:17:26 -0500

    daskify the resulting dataframe

--- Get_Stock_Market_Info.py
+++ Get_Stock_Market_Info.py
@@ -2,8 +2,8 @@
 # from libraries
 import requests
 import pandas as pd
-import json
-import os
+import dask.dataframe as dd
+import io
 import quandl
 import time
 
@@ -21,24 +21,29 @@ stock_symbols = sp_500_companies.ticker_list
 
 
 def get_alpha_vantage_data(symbols):
+    stock_info_list = []
     alpha_vantage_base = "https://www.alphavantage.co"  # url base
     param_dict_names = [name for name in dir(params) if ('params' in name)]
-    param_dict_keys = [key.replace('_params', '') for key in dir(params) if ('params' in key)]
-    param_dict = {key: getattr(params, name) for key in param_dict_keys for name in param_dict_names}
+    param_dict = {name: getattr(params, name) for name in param_dict_names}
+    param_dict = {name.replace('_params', ''): value for name, value in param_dict.items()}
     for stock_symbol in symbols:
+        stock_attribute_df_list = []
         print('Getting data for {}...'.format(stock_symbol))
         for param_name, param_value in param_dict.items():
             print("Process Begin...")
             param_value["symbol"] = stock_symbol
-            r = requests.get(alpha_vantage_base + '/query', params=param_value)
-            content = json.loads(r.content)
-            fund_prefix = ''.join((stock_symbol, "_"))
-            file_name = ''.join((fund_prefix, (param_name + '.json')))
-            with open(os.path.join(raw_data_folder, file_name), "w") as fp:
-                json.dump(content, fp)
-            print("Successfully extracted {}!".format(file_name))
+            r = requests.get(alpha_vantage_base + '/query', params=param_value, headers=auth.user_agent)
+            df = pd.read_csv(io.StringIO(r.content.decode('utf-8'))).set_index('timestamp')
+            df["stock_symbol"] = stock_symbol
+            dask_df = dd.from_pandas(df, npartitions=)
+            stock_attribute_df_list.append(dask_df)
+
+            print("Successfully extracted {}!".format(param_name))
             print("Process halting...")
             time.sleep(20)
+        stock_attribute_dask = dd.concat(stock_attribute_df_list, axis=1)
+        stock_info_list.append(stock_attribute_dask)
+    sp_500_data = dd.concat(stock_info_list, axis=0)
         print("Data extraction for {} complete!".format(stock_symbol))
 
     print('Alpha Vantage Data Extraction Process Complete!!')
