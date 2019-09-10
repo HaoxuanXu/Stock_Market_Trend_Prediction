b5a65f304b78792888676d5a775245fc3e17149b
HaoxuanXu <haoxuanxu@uchicago.edu>
2019-09-10 01:24:30 -0500

    finalizing dask fix

--- data_extraction_pipeline.py
+++ data_extraction_pipeline.py
@@ -14,8 +14,8 @@ ticker_list = sp500.get_sp_500(url=sp500.url, headers=auth.user_agent)
 quandl_parameters = [a for a in dir(params) if ("_parameters" in a)]
 
 
-alpha_vantage_data = get_data_util.get_alpha_vantage_data(ticker_list[:2])
-quandl_data = get_data_util.get_quandl_data(quandl_parameters[:3])
+alpha_vantage_data = get_data_util.get_alpha_vantage_data(ticker_list)
+quandl_data = get_data_util.get_quandl_data(quandl_parameters)
 
 #alpha_vantage_data.to_csv(temp_file_folder+"temp_alpha.csv")
 #quandl_data.to_csv(temp_file_folder+"temp_quandl.csv")
@@ -24,6 +24,7 @@ stock_market_data = dd.merge(alpha_vantage_data, quandl_data)
 #stock_market_data = alpha_vantage_data.join(quandl_data, how="outer")
 #stock_market_data_pickle = stock_market_data.copy()
 #stock_market_data_pickle.to_pickle(temp_file_folder+"temp_data_file.pkl")
-stock_market_data.to_csv(temp_file_folder+"temp_data_file.csv")
+#stock_market_data.to_csv(temp_file_folder+"temp_data_file.csv")
+print(stock_market_data)
 
 #get_data_util.write_data_to_s3(df=stock_market_data, bucket_name=auth.bucket_name, file_name="stock_market_data")
