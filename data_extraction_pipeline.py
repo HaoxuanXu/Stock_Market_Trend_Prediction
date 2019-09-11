1ad6b6d4ad9c430ee703679470156a17954612cc
HaoxuanXu <haoxuanxu@uchicago.edu>
2019-09-11 14:54:06 -0500

    retrieving dow jones data

--- data_extraction_pipeline.py
+++ data_extraction_pipeline.py
@@ -6,10 +6,13 @@ import params
 import auth
 import get_data_util
 import get_sp500_util as sp500
+import get_dow_jones_util as dj
 
 temp_file_folder = "C:/Users/John Xu/Desktop/Stock_Market_Trend_Prediction/temp_file/"
+
 # list of S&P 500 companies tickers
-ticker_list = sp500.get_sp_500(url=sp500.url, headers=auth.user_agent)
+ticker_list = dj.get_dow_jones(url=dj.url, headers=auth.user_agent)
+
 # Names of parameters to be extracted from Quandl
 quandl_parameters = [a for a in dir(params) if ("_parameters" in a)]
 
@@ -17,16 +20,12 @@ quandl_parameters = [a for a in dir(params) if ("_parameters" in a)]
 alpha_vantage_data = get_data_util.get_alpha_vantage_data(ticker_list)
 quandl_data = get_data_util.get_quandl_data(quandl_parameters)
 
-#alpha_vantage_data.to_csv(temp_file_folder+"temp_alpha.csv")
-#quandl_data.to_csv(temp_file_folder+"temp_quandl.csv")
+
 
 print("Begin merging Alpha Vantage and Quandl Data...")
-stock_market_data = dd.merge(alpha_vantage_data, quandl_data)
+stock_market_data = alpha_vantage_data.join(quandl_data)
 print("Merge Complete!!!")
-#stock_market_data = alpha_vantage_data.join(quandl_data, how="outer")
-#stock_market_data_pickle = stock_market_data.copy()
-#stock_market_data_pickle.to_pickle(temp_file_folder+"temp_data_file.pkl")
-#stock_market_data.to_csv(temp_file_folder+"temp_data_file.csv")
 
 
-get_data_util.write_data_to_s3(df=stock_market_data, bucket_name=auth.bucket_name, file_name="stock_market_data")
+
+get_data_util.write_data_to_s3(df=stock_market_data, bucket_name=auth.bucket_name, file_name="dow_jones_stock_market_data")
--- get_data_util.py
+++ get_data_util.py
@@ -9,6 +9,7 @@ from io import StringIO
 import quandl
 import time
 import boto3
+import datetime
 
 # from files:
 import auth
@@ -28,34 +29,34 @@ def get_alpha_vantage_data(symbols):
     begin_time = time.perf_counter()
     stock_info_list = []
     alpha_vantage_base = "https://www.alphavantage.co"  # url base
-    param_dict_names = [name for name in dir(params) if ('params' in name)]
+    param_dict_names = [name for name in dir(params) if ('_params' in name)]
     param_dict_list = [getattr(params, name) for name in param_dict_names]
     for stock_symbol in symbols:
-        start = time.perf_counter()
         stock_indicators = []
+        start = time.perf_counter()
         for i in range(len(param_dict_list)):
-            param_dict_list[i]["symbol"] = stock_symbol
             session = requests.Session()
             retry = Retry(connect=5, backoff_factor=0.5)
             adapter = HTTPAdapter(max_retries=retry)
             session.mount("http://", adapter)
             session.mount("https://", adapter)
+            param_dict_list[i]["symbol"] = stock_symbol
             r = session.get(alpha_vantage_base + '/query', params=param_dict_list[i], headers=auth.user_agent)
-            df = pd.read_csv(StringIO(r.content.decode('utf-8')), sep=",", engine="python")
+            df = pd.read_csv(StringIO(r.content.decode('utf-8')), engine="python").loc[1:,:]
+            df.columns = [param_dict_list[i]["function"] + "_" + str(column) for column in df.columns]
             df.set_index(df.columns[0], inplace=True)
-            ddf = dd.from_pandas(df, npartitions=4)
-            del df
-            ddf.columns = [param_dict_list[i]["function"] + "_" + str(column) for column in ddf.columns]
             if param_dict_list[i]["function"] == "TIME_SERIES_DAILY_ADJUSTED":
-                ddf["stock_symbol"] = stock_symbol
-            stock_indicators.append(ddf)
-            print("Successfully extracted {}!".format(stock_symbol+"_"+param_dict_names[i].replace("_params","")))
-        stock_info = dd.concat(stock_indicators, axis=1)
+                df["stock_symbol"] = stock_symbol
+            stock_indicators.append(df)
+            print("Successfully extracted {}!".format(stock_symbol + "_" + param_dict_names[i].replace("_params", "")))
+        stock_info = pd.concat(stock_indicators, axis=1, sort=False)
+        stock_info = stock_info.loc[:, ~stock_info.columns.duplicated()]
         stock_info_list.append(stock_info)
         runtime = time.perf_counter() - start
         print("Data extraction for {} complete! Runtime:{} seconds".format(stock_symbol, str(round(runtime, 2))))
-        print("Total Time Elapsed--{} seconds...".format(str(round(time.perf_counter()-begin_time, 2))))
-    concat_data = dd.concat(stock_info_list, axis=0)
+        print("Total Time Elapsed--{} seconds...".format(str(round(time.perf_counter() - begin_time, 2))))
+
+    concat_data = pd.concat(stock_info_list, axis=0, sort=False)
     total_runtime = time.perf_counter() - begin_time
     print('Alpha Vantage Data Extraction Process Complete!!  Total Runtime: {} seconds...'.format(str(total_runtime)))
     return concat_data
@@ -68,22 +69,20 @@ def get_quandl_data(quandl_params):  # indicators are a list of the names of ext
     print("Begin Retrieving Quandl Data...")
     for param in quandl_params:
         dict_ = getattr(params, param)
-        data = dd.from_pandas(quandl.get("{}/{}".format(dict_["database_code"], dict_["dataset_code"]),
-                          start_date=dict_["start_date"], end_date=dict_["end_date"]), npartitions = 4)
-        #data = data.add_prefix(param.split("_")[0]+"_")
-        data.columns = [param.split("_")[0]+"_"+str(column) for column in data.columns]
+        data = quandl.get("{}/{}".format(dict_["database_code"], dict_["dataset_code"]),
+                          start_date=dict_["start_date"], end_date=dict_["end_date"])
+        # data = data.add_prefix(param.split("_")[0]+"_")
+        data.columns = [param.split("_")[0] + "_" + str(column) for column in data.columns]
         indicator_data_list.append(data)
-        print("Retrieved Data on {}! Time Elapsed: {} seconds".format(param.replace("_parameters",""),
-                                                                      str(round(time.perf_counter()-begin_time))))
-    indicator_data = dd.concat(indicator_data_list, axis=1)
+        print("Retrieved Data on {}! Time Elapsed: {} seconds".format(param.replace("_parameters", ""),
+                                                                      str(round(time.perf_counter() - begin_time))))
+    indicator_data = pd.concat(indicator_data_list, axis=1, sort=False)
     print("Process Complete!!!")
     return indicator_data
 
 
 def write_data_to_s3(df, bucket_name, file_name):
     csv_buffer = StringIO()
-    df.compute().to_csv(csv_buffer)
+    df.to_csv(csv_buffer)
     s3_resource = boto3.resource('s3')
     s3_resource.Object(bucket_name, '{}.csv'.format(file_name)).put(Body=csv_buffer.getvalue())
-
-
